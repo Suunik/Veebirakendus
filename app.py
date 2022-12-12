f5a9d5bd8433b10d6fc77dbf577013722b6d2478
Suunik <91889117+Suunik@users.noreply.github.com>
2022-12-12 22:15:11 +0200

    First app push

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def avaleht():  # put application's code here
+    return render_template("avaleht.html")
+
+
+@app.route('/Osaühingu_andmete_vaade')
+def osaühingu_andmete_vaade():
+    return render_template("Osaühingu_andmete_vaade.html")
+
+
+@app.route('/Osaühingu_asutamise_vorm')
+def osaühingu_asutamise_vorm():
+    return render_template("Osaühingu_asutamise_vorm.html")
+
+
+if __name__ == '__main__':
+    app.run()
